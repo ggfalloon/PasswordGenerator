f76c11403ea597d6cc713c29e648dbe9208a74b8
Gabriele Falloon <ggfalloon@gmail.com>
2020-10-29 17:21:05 -0500

    updated character type arrays

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var generateBtn = document.querySelector("#generate");
-var lowerCase = ["a", "b", "c",]
-var uppperCase = ["A", "B", "C"]
-var numbers = ["1", "2", "3"]
-var specChar = ["%", "$", "*"]
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var uppperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+var specChar = ["%", "$", "*", "!", "@", "#", "^", "&", "*"]
 var options = []
 
 function generatePassword() {
@@ -19,14 +19,14 @@ function generatePassword() {
   if (wantsLowerCase) {
     options = lowerCase.concat(options)
   }
-  if (wantsLowerCase) {
-    options = lowerCase.concat(options)
+  if (wantsUpperCase) {
+    options = uppperCase.concat(options)
   }
-  if (wantsLowerCase) {
-    options = lowerCase.concat(options)
+  if (wantsNumbers {
+    options = numbers.concat(options)
   }
-  if (wantsLowerCase) {
-    options = lowerCase.concat(options)
+  if (wantsSpecialChar) {
+    options = specChar.concat(options)
   }
   if (wantsLowerCase) {
     options = lowerCase.concat(options)
