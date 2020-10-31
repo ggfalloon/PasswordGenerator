f1505b978c11e026812bc16c823d4df27dda83d0
Gabriele Falloon <ggfalloon@gmail.com>
2020-10-31 10:05:08 -0500

    fixed Math floor to remove NaN

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Add event listener to generate button
 var generateBtn = document.querySelector("#generate");
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var uppperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
@@ -5,7 +6,7 @@ var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 var specChar = ["%", "$", "*", "!", "@", "#", "^", "&", "*"]
 var options = []
 
-// Add event listener to generate button
+
 function generatePassword() {
 
   var charLength = prompt("How many characters would you like to include? Please choose a number between 8 - 128");
@@ -28,16 +29,13 @@ function generatePassword() {
   if (wantsSpecialChar) {
     options = specChar.concat(options)
   }
-  else if (wantsLowerCase && wantsUpperCase && wantsNumbers && wantsSpecialChar === false) {
-    alert("Please choose at least one character type.");
-  }
-  // loop for random characters amount
+
+  // Loop for random characters amount
   for (var i = 0; i <= charLength; i++) {
-    password = password + Math.floor(Math.random() * Math.floor[options.length - 1]);
+    password = password + options[Math.floor(Math.random() * options.length)];
   }
   return password;
 }
-
 
 // Write password to the #password input
 function writePassword() {
